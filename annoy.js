ed9a82fb22f72826d865cea4d7b3e68b491285b2
nizjack09 <nizjack09@gmail.com>
2021-02-28 17:16:47 +0200

    hello commit

---
+++ annoy.js
@@ -0,0 +1,17 @@
+
+// while(secretWord !== "yes" || secretWord !== "yeah"){
+//     let secretWord = ("Enter The SecretWord Guys!!!!!!!!");  
+    
+// }
+// alert("YAYAYA, YOU MADE IT");
+let secretWord = prompt("Enter The SecretWord Guys!!!!!!!!");
+if(secretWord =="yes"||secretWord=="yeah" ){
+    alert("yeah , you win");
+}
+while (secretWord !=="yes"||secretWord!=="yeah"){
+    let secretWord = prompt("try again brooooooooo!!!!!!!!");
+          if(secretWord =="yes"||secretWord=="yeah" ){
+           alert("yeah , you win");
+   }
+
+}
